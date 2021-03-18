ee9793ac9b7b85c595d567b0dea8fe8bffce7c69
Dell-Gustavo\gutoc <guto.contreiras@gmail.com>
2021-03-18 19:24:47 -0300

    Little fix

--- web/constants.php
+++ web/constants.php
@@ -11,7 +11,7 @@ class Constants {
 
     const API_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJmZWVnb3ciLCJhdWQiOiJwdWJsaWNhcGkiLCJpYXQiOiIxNy0wOC0yMDE4IiwibGljZW5zZUlEIjoiMTA1In0.UnUQPWYchqzASfDpVUVyQY0BBW50tSQQfVilVuvFG38";
 
-    const ERR_MSG_PROFESSIONALS_LIST = "Não foi possível obter as os profissionais atuantes na clínica!";
+    const ERR_MSG_PROFESSIONALS_LIST = "Não foi possível obter os profissionais atuantes na clínica!";
     const ERR_MSG_SPECIALTIES_LIST = "Não foi possível obter as especialidades disponíveis na clínica!";
     const ERR_MSG_PATIENTS_LIST = "Não foi possível obter os pacientes da clínica!!";
 }
